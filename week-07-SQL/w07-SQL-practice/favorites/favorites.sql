6bf3fdfe52373eb8adec0b133bf26487e37f3eaa
ITurres <emanueliturres.co@gmail.com>
2023-02-24 20:49:34 -0300

    Add favorites solution

---
+++ week-07-SQL/w07-SQL-practice/favorites/favorites.sql
@@ -0,0 +1,160 @@
+UPDATE
+    shows
+SET
+    title = "How I Met Your Mother"
+WHERE
+    title = "How i met your mother";
+
+UPDATE
+    shows
+SET
+    title = "Adventure Time"
+WHERE
+    title LIKE "adventure time";
+
+UPDATE
+    shows
+SET
+    title = "Avatar: The Last Airbender"
+WHERE
+    title LIKE "Avata%";
+
+UPDATE
+    shows
+SET
+    title = "Brooklyn Nine-Nine"
+WHERE
+    title LIKE "brooklyn%";
+
+UPDATE
+    shows
+SET
+    title = "Community"
+WHERE
+    title = "community";
+
+UPDATE
+    shows
+SET
+    title = "Family Guy"
+WHERE
+    title = "family guy";
+
+UPDATE
+    shows
+SET
+    title = "Friends"
+WHERE
+    title = "friends";
+
+UPDATE
+    shows
+SET
+    title = "Game of Thrones"
+WHERE
+    title LIKE "game%";
+
+UPDATE
+    shows
+SET
+    title = "Gilmore Girls"
+WHERE
+    title LIKE "gilmore g%";
+
+UPDATE
+    shows
+SET
+    title = "Grey's Anatomy"
+WHERE
+    title LIKE "grey'%";
+
+UPDATE
+    shows
+SET
+    title = "It's Always Sunny in Philadelphia"
+WHERE
+    title LIKE "its%";
+
+UPDATE
+    shows
+SET
+    title = "Parks and Recreation"
+WHERE
+    title LIKE "parks%";
+
+UPDATE
+    shows
+SET
+    title = "Sherlock"
+WHERE
+    title LIKE "sherlock%";
+
+UPDATE
+    shows
+SET
+    title = "Squid Game"
+WHERE
+    title = "squid game";
+
+UPDATE
+    shows
+SET
+    title = "The Bachelorette"
+WHERE
+    title LIKE "the bachelore%";
+
+UPDATE
+    shows
+SET
+    title = "The Crown"
+WHERE
+    title LIKE "the crown";
+
+UPDATE
+    shows
+SET
+    title = "The Office"
+WHERE
+    title LIKE "the office";
+
+UPDATE
+    shows
+SET
+    title = "The Office"
+WHERE
+    title LIKE "thevoffice";
+
+UPDATE
+    shows
+SET
+    title = "The Queen’s Gambit"
+WHERE
+    title LIKE "%gambit";
+
+UPDATE
+    shows
+SET
+    title = "The Untamed"
+WHERE
+    title LIKE "%untamed";
+
+UPDATE
+    shows
+SET
+    title = "The Office"
+WHERE
+    title LIKE "%office";
+
+UPDATE
+    shows
+SET
+    title = "Billions"
+WHERE
+    title = "billions";
+
+UPDATE
+    shows
+SET
+    title = "Criminal Minds"
+WHERE
+    title LIKE "criminal%";
